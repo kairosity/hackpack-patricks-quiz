4463d50cbcd196c2f818b4928a4585096928a82d
kairosity <karinafinegan@gmail.com>
2021-03-04 12:14:45 +0000

    registration/login routes & html set up done

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo, pymongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -23,6 +24,73 @@ users = mongo.db.users.find()
 def home():
     return render_template("index.html", users=users)
 
+@app.route("/register")
+def register():
+
+    if request.method == "POST":
+        existing_email = mongo.db.users.find_one(
+                    {"email": request.form.get("email").lower()})
+        existing_username = mongo.db.users.find_one(
+                        {"username": request.form.get("username").lower()})
+
+        if existing_email:
+            flash("Email is already registered.")
+            return redirect(url_for('register'))
+
+        if existing_username:
+            flash("Username is already in use, please choose a different one.")
+            return redirect(url_for('register'))
+
+        password1 = request.form.get("password")
+        password2 = request.form.get("password-confirmation")
+
+        if password1 != password2:
+            flash("Passwords do not match, please try again.")
+            return redirect(url_for('register'))
+
+        register_user = {
+        "first_name": request.form.get("first_name"),
+        "last_name": request.form.get("last_name"),
+        "username": request.form.get("username").lower(),
+        "email": request.form.get("email").lower(),
+        "county_team": request.form.get("county_team"),
+        "password": generate_password_hash(request.form.get("password")),
+        }
+
+        mongo.db.users.insert_one(register_user)
+
+        session["user"] = request.form.get("username").lower()
+        flash("Registration successful Welcome to the pub quiz!")
+        username = session["user"]
+
+    return render_template("registration.html")
+
+
+@app.route("/login")
+def login():
+
+    if request.method == "POST":
+        email = request.form.get("email").lower()
+        password = request.form.get("password")
+
+        existing_user = database_var.db.users.find_one(
+        {"email": email})
+
+        if existing_user:
+            if check_password_hash(
+                    existing_user["password"], password):
+                username = existing_user["username"]
+                session["user"] = username
+                flash(f"Welcome, {username}!")
+                return redirect(url_for("index"))
+            else:
+                flash("Incorrect username and/or password!")
+                return redirect(url_for("login"))
+        else:
+            flash("Incorrect username and/or password!")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
---
+++ static/js/script.js
@@ -0,0 +1,5 @@
+  // This code is for the dropdown in the regsitration form to select the county
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, options);
+  });
