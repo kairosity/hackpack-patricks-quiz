51058d9ee0ce46c2dc2a3c2104c9686f17d0e11a
David-A-Ray <Mr.David.A.Ray@gmail.com>
2021-03-05 15:05:15 +0000

    initial game layout and some starter JS added

---
+++ static/js/questions.js
@@ -0,0 +1,53 @@
+// St Patricks day multi questions
+const roundOneQuestions = [
+    {
+        question: "1Q",
+        options: ["1A", "1B", "1C"],
+        answer: "1B"
+    },
+    {
+        question: "2Q",
+        options: ["2A", "2B", "2C"],
+        answer: "2C"
+    },
+    {
+        question: "3Q",
+        options: ["3A", "3B", "3C"],
+        answer: "3B"
+    },
+    {
+        question: "4Q",
+        options: ["4A", "4B", "4C"],
+        answer: "4A"
+    },
+    {
+        question: "5Q",
+        options: ["5A", "5B", "5C"],
+        answer: "5A"
+    },
+    {
+        question: "6Q",
+        options: ["6A", "6B", "6C"],
+        answer: "6B"
+    },
+    {
+        question: "7Q",
+        options: ["7A", "7B", "7C"],
+        answer: "7C"
+    },
+    {
+        question: "8Q",
+        options: ["8A", "8B", "8C"],
+        answer: "8C"
+    },
+    {
+        question: "9Q",
+        options: ["9A", "9B", "9C"],
+        answer: "9B"
+    },
+    {
+        question: "10Q",
+        options: ["10A", "11B", "10C"],
+        answer: "10A"
+    },
+]
---
+++ static/js/quiz.js
@@ -0,0 +1,4 @@
+let score = 0;
+let Round1Score = 0;
+let Round2Score = 0;
+let Round3Score = 0;
