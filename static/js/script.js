5de519c92fd1fa929af0c23a5242abef7e36ca56
kairosity <karinafinegan@gmail.com>
2021-03-08 13:11:30 +0000

    leaderboard styles for small screens working

--- static/js/script.js
+++ static/js/script.js
@@ -8,7 +8,7 @@
     });
 
 
-$('.scoreboard:nth(0)').before('<div class="place col-2 gold">Gold:</div>');
-$('.scoreboard:nth(1)').before('<div class="place col-2 silver">Silver:</div>');
-$('.scoreboard:nth(2)').before('<div class="place col-2 bronze">Bronze:</div>');
+$('.scoreboard:nth(0)').before('<div class="place col-4 gold"><h4 class="gold-h4">Gold:</h4></div>');
+$('.scoreboard:nth(1)').before('<div class="place col-4 silver"><h4 class="silver-h4">Silver:</h4></div>');
+$('.scoreboard:nth(2)').before('<div class="place col-4 bronze"><h4 class="bronze-h4">Bronze:</h4></div>');
 $('.scoreboard:nth(3)').before('<hr id="hr_scores">');
