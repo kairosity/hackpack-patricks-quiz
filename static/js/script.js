402b1bb62c4b66a5b793c02ddfb7235f82e6b1e0
Claudio-C-Santos <cula.santos@gmail.com>
2021-03-08 09:05:10 +0000

    leaderboard styling

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,3 @@
-  // This code is for the dropdown in the regsitration form to select the county
-  $(document).ready(function(){
-    $('select').formSelect();
-  });
-
 // This adds the .active class to the navbar link of whatever page the user is on
    $(function(){
        $(".nav-link").each(function(){
@@ -11,3 +6,9 @@
                }
        });
     });
+
+
+$('.scoreboard:nth(0)').before('<div class="place col-2 gold">Gold:</div>');
+$('.scoreboard:nth(1)').before('<div class="place col-2 silver">Silver:</div>');
+$('.scoreboard:nth(2)').before('<div class="place col-2 bronze">Bronze:</div>');
+$('.scoreboard:nth(3)').before('<hr id="hr_scores">');
